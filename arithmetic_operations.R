c2fef49f79fe42f17da21dc09051e4adff94399d
Declan Davis <dexthefish@gmail.com>
2023-03-08 00:53:56 -0500

    add scripts

---
+++ arithmetic_operations.R
@@ -0,0 +1,15 @@
+# Basic Calculations in R
+
+x <- 10
+y <- 5
+z <- x + y
+print(z)    # Output: 15
+
+z <- x - y
+print(z)    # Output: 5
+
+z <- x * y
+print(z)    # Output: 50
+
+z <- x / y
+print(z)    # Output: 2
---
+++ functions.R
@@ -0,0 +1,9 @@
+# Functions in R
+
+my_function <- function(x, y) {
+    z <- x + y
+    return(z)
+}
+
+result <- my_function(5, 10)
+print(result)    # Output: 15
